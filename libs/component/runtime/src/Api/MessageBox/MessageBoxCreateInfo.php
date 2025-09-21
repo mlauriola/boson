2db92eaaa30e28fb2670d6a8bd08518768b7dfcb
Kirill Nesmeyanov <nesk@xakep.ru>
2025-09-21 03:28:35 +0300

    Improve message box config

--- libs/component/runtime/src/Api/MessageBox/MessageBoxCreateInfo.php
+++ libs/component/runtime/src/Api/MessageBox/MessageBoxCreateInfo.php
@@ -7,8 +7,8 @@ namespace Boson\Api\MessageBox;
 final readonly class MessageBoxCreateInfo
 {
     public function __construct(
-        public string $title,
-        public string $text,
+        public string $title = '',
+        public string $text = '',
         public bool $cancel = false,
         public ?MessageBoxIcon $icon = null,
     ) {}
